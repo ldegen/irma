e8045e919a80c72dd98b6050c83e00414f6a82dd
Lukas Degener <l.degener@tarent.de>
2018-04-19 18:40:43 +0200

    mark-deps-bundled.js: add newline before eof

--- mark-deps-bundled.js
+++ mark-deps-bundled.js
@@ -2,4 +2,4 @@ var fs = require("fs");
 var path = require("path");
 var p = require("./package.json");
 p.bundleDependencies = Object.keys(p.dependencies);
-fs.writeFileSync(path.join(__dirname, 'package.json'), JSON.stringify(p, null, '  '));
+fs.writeFileSync(path.join(__dirname, 'package.json'), JSON.stringify(p, null, '  ')+"\n");
